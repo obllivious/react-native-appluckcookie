ed4577e5b177fca9912d475bae8a07d65a742403
Renato Fernandes <renatofsousa@gmail.com>
2018-11-01 20:44:20 -0300

    Add new files, updated App.js and README

--- App.js
+++ App.js
@@ -1,58 +1,113 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Text,
+    View,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class Botao extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+        
+        this.estilo = StyleSheet.create({ // Classe criada para usar em todo o app para botão com um estilo definido. 
+            
+            botao: {
+                width: 250,
+                heigth: 50,
+                borderWidth: 2,
+                borderColor: props.color,
+                backgroundColor: 'transparent',
+                borderRadius: 25,
+            },
+
+            botaoArea: {
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
+
+            botaoText: {
+                color: props.color,
+                fontSize: 14,
+                fontWeight: 'bold',
+            },
+
+        });
+    }
+
+    render() {
+        return (
+            
+            <TouchableOpacity style = {this.estilo.botao} onPress = {this.props.onPress}>
+
+                <View style = {this.estilo.botaoArea}>
+
+                    <Text style = {this.estilo.botaoText}>this.props.text</Text>
+
+                </View>
+
+            </TouchableOpacity>
+        );
+    }
+}
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {texto: ''};
+
+        this.frases = ['Nem tudo é nada.', 'Deus ajuda quem cedo madruga.', 'A maior de todas as torres começa do solo.'];
+
+        this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
+    }
+
+    quebrarBiscoito() {
+        let s = this.state;
+
+        let r = Math.floor(Math.random() * this.frases.length);
+
+        s.texto = this.frases[r];
+        this.setState(s);
+
+    }
+  
+    render() {
+        return (
+
+            <View style = {styles.body}>
+                
+                <Image sourse = {require('./images/cookie.png')} />
+                <Text style = {styles.texto}>"{this.state.texto}"</Text>
+                <Botao color = "#B59619" text = "Quebrar Biscoito" onPress = {this.quebrarBiscoito} />
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
+    body: {
+        paddingTop: 20,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    texto: {
+        fontSize: 17,
+        color: '#B59619',
+        margin: 30,
+        fontStyle: 'italic',
+        textAlign: 'center',
+    }
+
 });
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Add files and updated README<br/>
+Add new files and updated App.js<br/>
+
